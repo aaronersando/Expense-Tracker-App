5fd16b6036467dbf342d9e05162d3c8359493b72
aaronersando <agersando@gmail.com>
2025-07-19 19:15:39 +0800

    Add navigation setup and screens for expense management

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpenses from "./screens/ManageExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpenseOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    </BottomTabs.Navigator>
+  );
+}
+
+function App() {
   return (
     <>
       <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpenseOverview" component={ExpenseOverview} />
+          <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({});
