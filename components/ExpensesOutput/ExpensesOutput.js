f2237f90f22645a408551a9bd8f2e64b72e589a2
aaronersando <agersando@gmail.com>
2025-07-19 21:51:10 +0800

    Add ExpenseItem component and integrate it into ExpensesList; enhance ExpensesOutput and ExpensesSummary with styling and functionality

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,7 +1,8 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -68,9 +69,19 @@ const DUMMY_EXPENSES = [
 
 export default function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    paddingBottom: 0,
+
+    backgroundColor: GlobalStyles.colors.primary700,
+    flex: 1,
+  },
+});
