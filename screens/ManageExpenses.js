15e726028cf9113189ca3975d309eec8afaa4a9f
aaronersando <agersando@gmail.com>
2025-07-20 21:42:01 +0800

    Add Button component and integrate it into ManageExpenses; implement delete, cancel, and confirm functionality

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 export default function ManageExpenses({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -11,9 +14,60 @@ export default function ManageExpenses({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  function handleDeleteExpense() {
+    navigation.goBack();
+  }
+  function handleCancel() {
+    navigation.goBack();
+  }
+  function handleConfirm() {
+    navigation.goBack();
+  }
+
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode={"flat"} onPress={handleCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={handleConfirm}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon={"trash"}
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={handleDeleteExpense}
+          />
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
